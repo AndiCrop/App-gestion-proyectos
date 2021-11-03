995e73bb2dc45abf7d1e5ec3a767d59ca03369b2
AndiCrop <andrescamargo.co@hotmail.com>
2021-11-03 13:59:04 -0500

    configurar .document

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+            // crear script de fontawsome
+            <script src="https://kit.fontawesome.com/d4cd33a1a3.js" crossOrigin="anonymous"></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
